f0a7e4645d7f0ddd04fa226376b6a0dab7aea5af
emiliopardo <e.pardo.perez@gmail.com>
2021-05-12 13:46:51 +0200

    se añade plugin de ejemplo para ver su diseño y funcionamiento

--- test/test.js
+++ test/test.js
@@ -2,8 +2,13 @@ import Geostats from 'facade/geostats';
 
 const map = M.map({
   container: 'mapjs',
+  controls: ['panzoombar','scale','location','layerswitcher','mouse']
 });
 
-const mp = new Geostats();
-
+const mp = new Geostats('parametro 1');
 map.addPlugin(mp);
+
+const add = new M.plugin.AddLayers();
+map.addPlugin(add);
+
+
